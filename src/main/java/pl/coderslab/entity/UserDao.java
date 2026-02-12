337a8c267424ff9cb667778e80ac20bb89b468b7
Zuzanna-Zarniewska <zzarniew@visa.com>
2026-02-12 17:50:25 +0100

    database + key classes

---
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -0,0 +1,7 @@
+package pl.coderslab.entity;
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+
+}

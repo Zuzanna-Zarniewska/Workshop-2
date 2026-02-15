8cb677fa97bccf184c8f924b868c0942fd17b14c
Zuzanna-Zarniewska <zzarniew@visa.com>
2026-02-15 18:40:47 +0100

    small adjustments

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -6,17 +6,17 @@ import pl.coderslab.entity.UserDao;
 public class Main {
     public static void main(String[] args) {
         UserDao dao = new UserDao();
-        User u1 = dao.create(new User("user1", "user1@mail.com", "password1"));
-        User u2 = dao.create(new User("user2", "user2@mail.com", "password2"));
-        User u3 = dao.create(new User("user3", "user3@somemail.com", "password3"));
-        User u4 = dao.create(new User("user4", "user4@gmail.com", "password4"));
-        User u5 = dao.create(new User("user5", "user5@gmail.com", "password5"));
+//        User u1 = dao.create(new User("user1", "user1@mail.com", "password1"));
+//        User u2 = dao.create(new User("user2", "user2@mail.com", "password2"));
+//        User u3 = dao.create(new User("user3", "user3@somemail.com", "password3"));
+//        User u4 = dao.create(new User("user4", "user4@gmail.com", "password4"));
+//        User u5 = dao.create(new User("user5", "user5@gmail.com", "password5"));
 
-        User userRead = dao.read(15);
+        User userRead = dao.read(18);
         userRead.printInfo();
         userRead.changeData("newUser", "newuser@mail.com", "newPassword");
         dao.update(userRead);
-        dao.delete(14);
+        dao.delete(19);
 
         User[] allUsers = dao.findAll();
         System.out.println();
